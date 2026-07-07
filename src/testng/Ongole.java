7a910572a730d48d556014855a7c710ce1e9ce41
jesykumara <jesykumara@gmail.com>
2026-07-07 08:47:44 +0530

    Ongole

--- src/testng/Ongole.java
+++ src/testng/Ongole.java
@@ -1,9 +1,11 @@
 package testng;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
 
 public class Ongole {
+	WebDriver driver;
   @Test
   public void f() {
   }
